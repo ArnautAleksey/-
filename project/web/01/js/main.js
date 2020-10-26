39e302ba405a1e250db164cec13903e77273d3d4
AlexeyArnaut <96_arnaut.aleksey_69@mail.ru>
2020-10-26 16:54:59 +0500

    Исправил ошибки

--- project/web/01/js/main.js
+++ project/web/01/js/main.js
@@ -1,6 +1,21 @@
 const header = document.querySelector('.header');
 const button = document.querySelector('.menu__button');
 const menu = document.querySelector('.burger-menu');
+const links = document.querySelectorAll('.header__link')
+const body = document.querySelector('body')
+
+links.forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault()
+        
+        const href = link.getAttribute('href')
+        
+        document.querySelector(href).scrollIntoView({
+          behavior: 'smooth',
+          block: 'start'
+        })
+      })
+})
 
 window.onscroll = () => {
     if(window.pageYOffset > 100) {
@@ -13,4 +28,10 @@ window.onscroll = () => {
 button.onclick = () => {
     button.classList.toggle('menu__button_active')
     menu.classList.toggle('burger-menu_active')
+    if(button.classList.contains('menu__button_active')) {
+        body.style.overflow = 'hidden'
+    } else {
+        body.style.overflow = 'visible'
+    }
 }
+
