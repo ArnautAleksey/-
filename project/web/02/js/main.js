19a8844daea4cb1edf807b081d61161608c70bd2
AlexeyArnaut <96_arnaut.aleksey_69@mail.ru>
2020-11-02 20:58:59 +0500

    Сделал слайдер. Закончил сайт MoGo

--- project/web/02/js/main.js
+++ project/web/02/js/main.js
@@ -4,6 +4,23 @@ const links = document.querySelectorAll('.header__nav-link');
 const linkMine = document.querySelector('.header__logo-link');
 const body = document.querySelector('body');
 const accordeons = document.querySelectorAll('.wedo__accordion-item');
+let position = 0;
+const wedoContainer = document.querySelector('.wedo__slider-container');
+const wedoTrack = document.querySelector('.wedo__slider-track');
+const wedoItems = document.querySelectorAll('.wedo__slider-item');
+const wedoBtnPrev = document.querySelector('.wedo__button-prev');
+const wedoBtnNext = document.querySelector('.wedo__button-next');
+const wedoItemsCount = wedoItems.length
+const wedoItemWidth = wedoContainer.clientWidth / 1;
+const wedoMovePosition = 1 * wedoItemWidth;
+const workContainer = document.querySelector('.works__slider-container');
+const workTrack = document.querySelector('.works__slider-track');
+const workItems = document.querySelectorAll('.works__slider-item');
+const workBtnPrev = document.querySelector('.works__button-prev');
+const workBtnNext = document.querySelector('.works__button-next');
+const workItemsCount = workItems.length
+const workItemWidth = workContainer.clientWidth / 1;
+const workMovePosition = 1 * workItemWidth;
 
 stickyHeader();
 
@@ -30,7 +47,6 @@ menu.onclick = () => {
         body.style.overflow = 'visible';
     };
 };
-
 
 // Active links toggle & moving links & return to the main
 links.forEach(link => {
@@ -82,3 +98,53 @@ accordeons.forEach(accordeon => {
         accordeon.classList.add('wedo__accordion-item_active');
     });
 });
+
+// Webo slider
+wedoItems.forEach(item => {
+    item.style.minWidth = `${wedoItemWidth}px`;
+});
+
+wedoBtnPrev.addEventListener('click', () => {
+    const itemsLeft = Math.abs(position) / wedoItemWidth;
+
+    position += itemsLeft >= 1 ? wedoMovePosition : itemsLeft * wedoItemWidth;
+
+    wedoSetPosition();
+});
+
+wedoBtnNext.addEventListener('click', () => {
+    const itemsLeft = wedoItemsCount - (Math.abs(position) + 1 * wedoItemWidth) / wedoItemWidth;
+
+    position -= itemsLeft >= 1 ? wedoMovePosition : itemsLeft * wedoItemWidth;
+
+    wedoSetPosition();
+});
+
+function wedoSetPosition() {
+    wedoTrack.style.transform = `translateX(${position}px)`;
+};
+
+// Work slider
+workItems.forEach(item => {
+    item.style.minWidth = `${workItemWidth}px`;
+});
+
+workBtnPrev.addEventListener('click', () => {
+    const itemsLeft = Math.abs(position) / workItemWidth;
+
+    position += itemsLeft >= 1 ? workMovePosition : itemsLeft * workItemWidth;
+
+    workSetPosition();
+});
+
+workBtnNext.addEventListener('click', () => {
+    const itemsLeft = workItemsCount - (Math.abs(position) + 1 * workItemWidth) / workItemWidth;
+
+    position -= itemsLeft >= 1 ? workMovePosition : itemsLeft * workItemWidth;
+
+    workSetPosition();
+});
+
+function workSetPosition() {
+    workTrack.style.transform = `translateX(${position}px)`;
+};
