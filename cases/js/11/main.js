b7e5d5bdfe19b855d6df0833a4f5fdf42df1489d
AlexeyArnaut <96_arnaut.aleksey_69@mail.ru>
2020-11-12 19:57:35 +0500

    Добавил генератор рандомного цвета (v2)

---
+++ cases/js/11/main.js
@@ -0,0 +1,41 @@
+const colors = document.querySelectorAll('.color');
+const refresh = document.querySelector('.refresh');
+const copied = document.querySelector('.copied');
+
+refresh.addEventListener('click', changeColor);
+
+function changeColor() {
+    colors.forEach(color => {
+        const hex = color.querySelector('.hex');
+
+        color.style.backgroundColor = randomColor();
+        hex.innerHTML = randomColor();
+
+        color.addEventListener('click', () => {
+
+            const input = document.createElement('input');
+            color.appendChild(input);
+            input.value = color.textContent;
+            input.select();
+            document.execCommand('copy');
+            color.removeChild(input);
+
+            if(color.timeout) {
+                clearTimeout(color.timeout);
+            };
+
+            copied.classList.add('copied_active');
+
+            color.timeout = setTimeout(() => {
+                color.timeout = null;
+                copied.classList.remove('copied_active');
+            },1000);
+        });
+    });
+};
+
+function randomColor() {
+    return '#' + Math.random().toString(16).substr(2,6);
+};
+
+window.onload = changeColor();
